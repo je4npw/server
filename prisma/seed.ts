8f25322b975c1a5709e4665fa538db0f323a40bb
je4npw <je4n.pw@gmail.com>
2022-11-04 12:00:45 -0300

    Ajustes Prisma DB

---
+++ prisma/seed.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john@example',
+      avatarUrl: 'https://github.com/je4npw.png',
+
+    }
+  })
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example Pool',
+      code: 'BOL123',
+      ownerId: user.id,
+      participants: {
+        create: {
+          userId: user.id,
+        }
+      }
+    }
+  })
+  await prisma.game.create({
+    data: {
+      date: '2022-12-01T21:39:50.021Z',
+      firstTeamCountryCode: 'AF',
+      secoundTeamCountryCode: 'BO'
+    }
+  })
+  await prisma.game.create({
+    data: {
+      date: '2022-12-02T21:39:50.021Z',
+      firstTeamCountryCode: 'BR',
+      secoundTeamCountryCode: 'AR',
+      guesses: {
+        create: {
+          firstTeamPoints: 3,
+          secoundTeamPoints: 1,
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+}
+main()
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import Fastify from "fastify"
 import cors from "@fastify/cors"
+import { z } from 'zod'
 import { PrismaClient } from "@prisma/client"
+import ShortUniqueId from "short-unique-id"
 
+interface Props {
+  title: string
+}
 const prisma = new PrismaClient({
   log: ['query']
 });
@@ -18,6 +23,24 @@ async function bootstrap() {
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count()
     return { count }
+  })
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+
+    const { title } = createPoolBody.parse(request.body)
+    const generate = new ShortUniqueId({ length: 6 })
+    const code = String(generate).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code
+      }
+    })
+    return reply.status(201).send({ code })
   })
 
   await fastify.listen({ port: 3333, /*host: '0.0.0.0' */ })
